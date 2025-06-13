79dd838f986862ced0e75eaa44c3e1be5addae89
Dilar Pardo <dilar.pardo@gmail.com>
2025-06-13 17:21:20 -0500

    009

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,9 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (env('APP_ENV') === 'production') 
-        { 
-            URL::forceScheme('https');
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https'); // ← ESTO SOLO FUNCIONA SI importaste URL
         }
     }
 }
